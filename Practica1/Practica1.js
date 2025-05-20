ad8913b4e4bab69c9ccf87312df27cf3e4f96473
Cam09089 <Cam09089@users.noreply.github.com>
2025-05-19 18:11:40 -0600

    ejercicio b

--- Practica1/Practica1.js
+++ Practica1/Practica1.js
@@ -1,4 +1,5 @@
 
+//ejercicio a
 var nombre = "Armando";
 const edad = 25;
 
@@ -7,3 +8,8 @@ nombre = "Ana Maria";
 const saludo = 'Hola, ${nombre}. Tienes ${edad} años.';
 console.log(saludo);
 
+//ejercicio b
+const cuadrado = (num) => {
+    return num * num;
+}   
+console.log(cuadrado(10));
