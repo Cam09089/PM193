77f83b566080917f3d23a8e0b0b7d56c5b391e70
Cam09089 <Cam09089@users.noreply.github.com>
2025-06-12 16:07:34 -0600

    Primer parte practica 4 Componentes Props y State

---
+++ blank/App.js
@@ -0,0 +1,34 @@
+/*ZONA 1: Importaciones */
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View, Button } from 'react-native';
+
+const Texto = (props) => {
+  const{contenido} = props;
+
+  return (
+    <Text>{contenido}</Text>
+  );
+}
+
+/*ZONA 2: Main*/ 
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Texto contenido='Hola'></Texto>
+      <Texto contenido='Mundo'></Texto>
+      <Texto contenido='React Native'></Texto>
+      <Button title='Presionar'> </Button>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+/*ZONA 3: Estilos- Estetica*/ 
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
