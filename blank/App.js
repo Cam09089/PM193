bc2b75b49f3718e2de9ca9e5b3254017f1d8e90c
Cam09089 <Cam09089@users.noreply.github.com>
2025-06-16 20:27:00 -0600

    ACTIVIDAD NO.6:STYLESHEET & FLEX terminada

--- blank/App.js
+++ blank/App.js
@@ -4,17 +4,19 @@ import { StyleSheet, Text, View, Button } from 'react-native';
 import React, { useState } from 'react';
 
 //componnente Propio Texto
-const Texto = () => {
+const Texto = ({style}) => {
   const[contenido,setContenido] = useState('Hola Mundo');
+
+
   const actualizarTexto = () => {
     setContenido('Estado Modificado');
   }
 
   return (
-    <Text onPress={actualizarTexto}>{contenido}</Text>
+    <Text style={[styles.Text, style]} onPress={actualizarTexto}>{contenido}</Text>
   )
 }
-
+//componente Propio Boton
 const Boton = () => {
   const[contenido,setContenido] = useState('Presionar');
   const actualizarBoton = () => {
@@ -34,12 +36,12 @@ export default function App() {
   return (
     <View style={styles.container}>
 
-      <Texto>Hola</Texto>
-      <Texto>Mundo</Texto>
-      <Texto>react</Texto>
-      <Texto>native</Texto>
-      <Boton> Presionarlo</Boton>
+      <Texto style={styles.morado}>Hola</Texto>
+      <Texto style={styles.verde}>Mundo</Texto>
+      <Texto style={styles.rojo}>react</Texto>
+    
       <StatusBar style="auto" />
+
     </View>
   );
 }
@@ -49,7 +51,19 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: 'flex-stretch',
+    justifyContent: 'space-around',
+ 
   },
+  Text: {
+    color: 'pink',
+    fontSize: 30,
+    height: 150,
+    width: 150,
+  },
+  // Colores personalizados
+  morado: {backgroundColor: 'purple'},
+  verde: {backgroundColor: 'green'},
+  rojo: {backgroundColor: 'red'},
 });
+
